dcc87584d76468fcbf30a8ea8714a0b9855d79c6
ubtvpn-eng <zolkinzzz@gmail.com>
2026-01-08 18:04:32 +0300

    Set base path to root

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
     // Замените на точное название вашего репозитория!
     // Если репозиторий называется 'ubtvpn1', то пишем '/ubtvpn1'
-    base: '/ubtvpn1',
+    // Если репозиторий называется 'ubtvpn-eng.github.io', то base не нужен (или '/')
+    base: '/',
 
     // Ваша ссылка на GitHub Pages
     site: 'https://ubtvpn-eng.github.io/ubtvpn1',
